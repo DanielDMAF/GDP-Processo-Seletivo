ace9aa0c4cbf86aa95451b27f46a8c196d1e8ded
DanielDMAF <139928702+DanielDMAF@users.noreply.github.com>
2023-09-03 12:33:57 -0300

    Update

--- GDP Processo Seletivo/Assets/Script/playerMovimento.cs
+++ GDP Processo Seletivo/Assets/Script/playerMovimento.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovimento : MonoBehaviour
@@ -9,45 +10,53 @@ public class playerMovimento : MonoBehaviour
     [SerializeField] LayerMask chaoLayer;
     [SerializeField] Transform chaoCheck;
 
-    private bool facingRight;
+    private Animator anim;
+
+    private bool facingRight = true;
     public float vel_Mov;
-    private float mov_hor;
+    private float mov_hor = 0;
 
     public float forca_pulo; //pulo
+    public bool Jumping;
     private float coyoteDelay = 0.2f; //Coyote time pulo
     private float coyoteTimer;
 
-    private float mov_vert;
+    private float mov_vert = 0;
+    private bool crouch;
+    private bool lookUp;
+
+    private bool reload;
+
+    
 
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        //Parte flip sprite:
-        if ((facingRight && mov_hor < 0) || (!facingRight && mov_hor > 0))
-        {
-            Flip();
-        }
         
         //Parte Pulo:
         if (NoChao())
         {
             coyoteTimer = coyoteDelay;
+            anim.SetBool("Jump", false);
+            Jumping = false;
         }
         else
         {
             coyoteTimer -= Time.deltaTime;
         }
-        if (Input.GetButtonDown("Jump") && coyoteTimer > 0) //pula com a adiçao dp coyote timer
+        if ((Input.GetButtonDown("Jump") && coyoteTimer > 0) && (!lookUp && !crouch && !reload)) //pula com a adiçao dp coyote timer
         {
             rb.velocity = new Vector2(rb.velocity.x, forca_pulo);
+            Jumping = true;
         }
         if (Input.GetButtonUp("Jump") && (rb.velocity.y > 0)) //faz o pulo ser menor caso solte o botao
         {
@@ -55,15 +64,56 @@ public class playerMovimento : MonoBehaviour
             coyoteTimer = 0f;
         }
 
-        //Parte pegar input dos movimentos
-        mov_hor = Input.GetAxis("Horizontal");
-        mov_vert = Input.GetAxis("Vertical");
+        //Tiro parte:
+        if (Input.GetKeyDown(KeyCode.Z)) 
+        {
+
+            anim.SetTrigger("Shoot"); //Animation
+        }
+
+        //Reload//Slash parte
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Slash();
+        }
+
+        //Input Horizontais:
+        lookUp = Input.GetKey(KeyCode.UpArrow);
+        crouch = Input.GetKey(KeyCode.DownArrow);
+
+        //Parte pegar input dos movimentos horizontais
+        if (!lookUp && !crouch && !reload)
+        {
+            mov_hor = Input.GetAxis("Horizontal");
+        }
+        else if (NoChao())
+        {
+            mov_hor = 0;
+        }
+        //Animations parte:
+        anim.SetFloat("Speed", Mathf.Abs(mov_hor));
+        if (Jumping)
+        {
+            anim.SetBool("Jump", true);
+        }
+        anim.SetBool("LookUP", lookUp);
+        anim.SetBool("Crouch", crouch);
+        anim.SetBool("LookDown", crouch);
     }
+
     private void FixedUpdate()
     {
         //Parte movimentar player horizontalmente
         rb.velocity = new Vector2(vel_Mov * mov_hor, rb.velocity.y);
+
+        //Parte flip sprite:
+        if ((facingRight && mov_hor < 0) || (!facingRight && mov_hor > 0))
+        {
+            Flip();
+        }
     }
+
+
     private bool NoChao()
     {
         return Physics2D.OverlapCircle(chaoCheck.position, 0.2f, chaoLayer);
@@ -71,5 +121,12 @@ public class playerMovimento : MonoBehaviour
     private void Flip()
     {
         facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+    private void Slash()
+    {
+        //anim.SetBool("Reload", true);
     }
 }
